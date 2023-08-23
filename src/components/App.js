412bdb0abdee10909f16d58dd0c995b630f900e9
shenzor <toppan.frederic@gmail.com>
2023-08-23 22:43:00 +0200

    react app et logements.json chargé

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "../assets/LOGO.png";
+import "../style/App.css";
 
 function App() {
   return (
@@ -8,7 +8,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p>{" "}
+        <h1>Hello World !!! </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
