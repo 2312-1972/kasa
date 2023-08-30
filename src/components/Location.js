27cca93f415c9eb4efe20b756653cf19feb6baac
shenzor <toppan.frederic@gmail.com>
2023-08-30 17:14:16 +0200

    reprise  home page

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,10 +1,11 @@
-import mask from "../assets/_Section 1.png";
+import mask from "../assets/Mask Group.png";
 import "../sass/main.css";
 
 function Location() {
   return (
     <div>
       <img className="mask" src={mask} alt="mask"></img>
+      <h1 className="title-banner">Chez vous, partout et ailleurs</h1>
     </div>
   );
 }
