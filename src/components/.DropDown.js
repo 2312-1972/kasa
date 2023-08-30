20f546f175c3a421d8017941d77dda706fa336b3
shenzor <toppan.frederic@gmail.com>
2023-08-30 12:46:04 +0200

    arrow vector about component ok

--- src/components/.DropDown.js
+++ src/components/.DropDown.js
@@ -1,19 +1,23 @@
 import "../sass/main.css";
-
+import vector from "../assets/Vector.png";
 function BlockValeurs() {
   return (
     <div>
       <div className="valeurs">
-        <p className="title-valeur">Fiabilité</p>{" "}
+        <p className="title-valeur">Fiabilité</p>
+        <img src={vector} className="arrow" alt="arrow" />
       </div>
       <div className="valeurs">
-        <p className="title-valeur">Service</p>{" "}
+        <p className="title-valeur">Service</p>
+        <img src={vector} className="arrow" alt="arrow" />
       </div>
       <div className="valeurs">
-        <p className="title-valeur">Respect</p>{" "}
+        <p className="title-valeur">Respect</p>
+        <img src={vector} className="arrow" alt="arrow" />
       </div>
       <div className="valeurs">
-        <p className="title-valeur">Sécurité</p>{" "}
+        <p className="title-valeur">Sécurité</p>
+        <img src={vector} className="arrow" alt="arrow" />
       </div>
     </div>
   );
