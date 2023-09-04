ffe038a386e84c5078e148e4b588fd4488c7d1f0
shenzor <toppan.frederic@gmail.com>
2023-09-04 16:30:01 +0200

    gestion final des mauvaises routes avec "*"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from "react";
 
 import "./style/index.css";
-import {Routes, Route } from "react-router-dom";
-import Home from "./components/Home"
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 import About from "./components/About";
 import Error404 from "./components/Error404";
 import Carrousel from "./components/Carrousel";
 
-
-//configuration des routes 
-function App () {
-    return(
-      <Routes>
+//configuration des routes
+function App() {
+  return (
+    <Routes>
       <Route path="/" element={<Home />} /> {/* Route vers la page d'accueil */}
       <Route path="/about" element={<About />} /> {/* Route vers About */}
-     <Route path="/error" element={<Error404 />} />  {/*   Route vers Error404 */}
-    <Route path="/carrousel" element={<Carrousel />}/> {/*  /> Route vers Carrousel*/}
-    </Routes> 
-      
-    )
+      <Route path="/carrousel" element={<Carrousel />} />{" "}
+      {/*  /> Route vers Carrousel*/}
+      {/* Route générique pour gérer les erreurs 404 */}
+      <Route path="*" element={<Error404 />} /> {/*   Route vers Error404 */}
+    </Routes>
+  );
 }
 
- export default App;
+export default App;
