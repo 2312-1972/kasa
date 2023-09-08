d9c690f3b70b6954a96f5b3156d36fe23c0b96b9
shenzor <toppan.frederic@gmail.com>
2023-09-08 10:01:14 +0200

    modif  logements.json picture par pictures général

---
+++ src/components/Logements.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../sass/main.css";
+import Carrousel from "./Carrousel";
+
+function FicheLogement() {
+  return (
+    <div>
+      <Carrousel />
+      <div></div>
+    </div>
+  );
+}
+
+export default FicheLogement;
