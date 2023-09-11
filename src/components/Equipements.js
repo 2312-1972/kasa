800f1005662b810c71e45b8d23f676b30352b0e7
shenzor <toppan.frederic@gmail.com>
2023-09-11 10:43:13 +0200

    equipement list fini

--- src/components/Equipements.js
+++ src/components/Equipements.js
@@ -4,16 +4,16 @@ const EquipementsList = ({ equipements }) => {
   const [equipementsList, setEquipementsList] = useState([]);
 
   useEffect(() => {
-    // Divisez la chaîne de texte en un tableau en utilisant la virgule comme délimiteur
+    // Divise la chaîne de texte en un tableau en utilisant la virgule comme délimiteur
     const parsedEquipements = equipements.split(',').map(item => item.trim());
     setEquipementsList(parsedEquipements);
-  }, [equipements]); // Réagissez aux changements de la prop equipements
+  }, [equipements]); 
 
   return (
     <div>
       <ul>
         {equipementsList.map((item, index) => (
-          <li key={index}>{item}</li>
+          <li className='ul' key={index}>{item}</li>
         ))}
       </ul>
     </div>
